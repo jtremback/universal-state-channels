6c474ec356da4c8a47753829cb7dbc4aa5f97e5e
Jehan Tremback <jehan.tremback@gmail.com>
2016-03-28 22:58:22 -0700

    added ethereum judge folder

---
+++ ethereum-judge/contracts/MetaCoin.sol
@@ -0,0 +1,61 @@
+contract MetaCoin {
+	mapping (address => uint) balances;
+
+	function MetaCoin() {
+		balances[tx.origin] = 10000;
+	}
+
+	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
+		if (balances[msg.sender] < amount) return false;
+		balances[msg.sender] -= amount;
+		balances[receiver] += amount;
+		return true;
+	}
+
+    function getBalance(address addr) returns(uint) {
+        return balances[addr];
+    }
+    
+    function getChannelState(bytes32 channelId) returns(bytes) {
+        return channels[channelId].state;
+    }
+    
+    mapping (bytes32 => Channel) public channels;
+    
+    struct Channel {
+        bytes32 channelId;
+        bytes32 pubkey0;
+        bytes32 pubkey1;
+        bytes state;
+        uint hold_period;
+        bytes32 fingerprint;
+        byte[64] signature0;
+        byte[64] signature1;
+        uint8 phase;
+    }
+    
+    function addChannel(
+        bytes32 channelId,
+        bytes32 pubkey0,
+        bytes32 pubkey1,
+        bytes state,
+        uint hold_period,
+        bytes32 fingerprint,
+        byte[64] signature0,
+        byte[64] signature1
+    ) {
+        Channel memory ch = Channel(
+            channelId,
+            pubkey0,
+            pubkey1,
+            state,
+            hold_period,
+            fingerprint,
+            signature0,
+            signature1,
+            0
+        );
+        
+        channels[channelId] = ch;
+    }
+}
